cf3f0dbfd3e0187911fef29497ff98c0f9801e87
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-06-30 00:41:40 +0200

    Add files via upload

---
+++ Introduction to Java/Assignment2.java
@@ -0,0 +1,16 @@
+/**
+* This program print JAVA.
+* @author Marie Dohnalová
+*
+*/
+
+public class Assignment2 {
+
+    public static void main(String[] args) {
+        System.out.println("    J    A    V     V    A");
+        System.out.println("    J   A A    V   V    A A");
+        System.out.println("J   J  AAAAA    V V    AAAAA");     
+        System.out.println(" J J  A     A    V    A     A");
+    }
+    
+}
