b344c7112d1a70158d40f6dc4e14b3bb61f55ef7
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-06-30 00:40:28 +0200

    Getting Started

---
+++ Introduction to Java/Assignment1.java
@@ -0,0 +1,8 @@
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Marie Dohnalová");
+        System.out.println("I am 17 years old");
+    }
+  
+} 
