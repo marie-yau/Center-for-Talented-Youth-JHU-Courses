1346d7e95b4e0eabc555cd7eb8afca7292d21974
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-07-22 08:34:29 +0200

    Add files via upload

---
+++ AP Computer Science/JavaProgram.java
@@ -0,0 +1,99 @@
+public class JavaProgram
+{
+     public static void main( String [] args )
+     {   
+         System.out.println("CHARACTERS AND THEIR ASCII INTEGER VALUES");
+         System.out.println("\n");
+         System.out.println("Uppercase alphabetic characters");
+         System.out.println("Character \t Value");
+         System.out.printf( "%c \t \t %d\n", 'A', ( (int) 'A' ) );
+         System.out.printf( "%c \t \t %d\n", 'B', ( (int) 'B' ) );
+         System.out.printf( "%c \t \t %d\n", 'C', ( (int) 'C' ) );
+         System.out.printf( "%c \t \t %d\n", 'D', ( (int) 'D' ) );
+         System.out.printf( "%c \t \t %d\n", 'E', ( (int) 'E' ) );
+         System.out.printf( "%c \t \t %d\n", 'F', ( (int) 'F' ) );
+         System.out.printf( "%c \t \t %d\n", 'G', ( (int) 'G' ) );
+         System.out.printf( "%c \t \t %d\n", 'H', ( (int) 'H' ) );
+         System.out.printf( "%c \t \t %d\n", 'I', ( (int) 'I' ) );
+         System.out.printf( "%c \t \t %d\n", 'J', ( (int) 'J' ) );
+         System.out.printf( "%c \t \t %d\n", 'K', ( (int) 'K' ) );
+         System.out.printf( "%c \t \t %d\n", 'L', ( (int) 'L' ) );
+         System.out.printf( "%c \t \t %d\n", 'M', ( (int) 'M' ) );
+         System.out.printf( "%c \t \t %d\n", 'N', ( (int) 'N' ) );
+         System.out.printf( "%c \t \t %d\n", 'O', ( (int) 'O' ) );
+         System.out.printf( "%c \t \t %d\n", 'P', ( (int) 'P' ) );
+         System.out.printf( "%c \t \t %d\n", 'Q', ( (int) 'Q' ) );
+         System.out.printf( "%c \t \t %d\n", 'R', ( (int) 'R' ) );
+         System.out.printf( "%c \t \t %d\n", 'S', ( (int) 'S' ) );
+         System.out.printf( "%c \t \t %d\n", 'T', ( (int) 'T' ) );
+         System.out.printf( "%c \t \t %d\n", 'U', ( (int) 'U' ) );
+         System.out.printf( "%c \t \t %d\n", 'V', ( (int) 'V' ) );
+         System.out.printf( "%c \t \t %d\n", 'W', ( (int) 'W' ) );
+         System.out.printf( "%c \t \t %d\n", 'X', ( (int) 'X' ) );
+         System.out.printf( "%c \t \t %d\n", 'Y', ( (int) 'Y' ) );
+         System.out.printf( "%c \t \t %d\n", 'Z', ( (int) 'Z' ) );
+         
+         System.out.println("\n");
+         System.out.println("Lowercase alphabetic characters");
+         System.out.println("Character \t Value");
+         System.out.printf( "%c \t \t %d\n", 'a', ( (int) 'a' ) );
+         System.out.printf( "%c \t \t %d\n", 'b', ( (int) 'b' ) );
+         System.out.printf( "%c \t \t %d\n", 'c', ( (int) 'c' ) );
+         System.out.printf( "%c \t \t %d\n", 'd', ( (int) 'd' ) );
+         System.out.printf( "%c \t \t %d\n", 'e', ( (int) 'e' ) );
+         System.out.printf( "%c \t \t %d\n", 'f', ( (int) 'f' ) );
+         System.out.printf( "%c \t \t %d\n", 'g', ( (int) 'g' ) );
+         System.out.printf( "%c \t \t %d\n", 'h', ( (int) 'h' ) );
+         System.out.printf( "%c \t \t %d\n", 'i', ( (int) 'i' ) );
+         System.out.printf( "%c \t \t %d\n", 'j', ( (int) 'j' ) );
+         System.out.printf( "%c \t \t %d\n", 'k', ( (int) 'k' ) );
+         System.out.printf( "%c \t \t %d\n", 'l', ( (int) 'l' ) );
+         System.out.printf( "%c \t \t %d\n", 'm', ( (int) 'm' ) );
+         System.out.printf( "%c \t \t %d\n", 'n', ( (int) 'n' ) );
+         System.out.printf( "%c \t \t %d\n", 'o', ( (int) 'o' ) );
+         System.out.printf( "%c \t \t %d\n", 'p', ( (int) 'p' ) );
+         System.out.printf( "%c \t \t %d\n", 'q', ( (int) 'q' ) );
+         System.out.printf( "%c \t \t %d\n", 'r', ( (int) 'r' ) );
+         System.out.printf( "%c \t \t %d\n", 's', ( (int) 's' ) );
+         System.out.printf( "%c \t \t %d\n", 't', ( (int) 't' ) );
+         System.out.printf( "%c \t \t %d\n", 'u', ( (int) 'u' ) );
+         System.out.printf( "%c \t \t %d\n", 'v', ( (int) 'v' ) );
+         System.out.printf( "%c \t \t %d\n", 'w', ( (int) 'w' ) );
+         System.out.printf( "%c \t \t %d\n", 'x', ( (int) 'x' ) );
+         System.out.printf( "%c \t \t %d\n", 'y', ( (int) 'y' ) );
+         System.out.printf( "%c \t \t %d\n", 'z', ( (int) 'z' ) );
+         
+         System.out.println("\n");
+         System.out.println("Digits");
+         System.out.println("Character \t Value");
+         System.out.printf( "%c \t \t %d\n", '0', ( (int) '0' ) );
+         System.out.printf( "%c \t \t %d\n", '1', ( (int) '1' ) );
+         System.out.printf( "%c \t \t %d\n", '2', ( (int) '2' ) );
+         System.out.printf( "%c \t \t %d\n", '3', ( (int) '3' ) );
+         System.out.printf( "%c \t \t %d\n", '4', ( (int) '4' ) );
+         System.out.printf( "%c \t \t %d\n", '5', ( (int) '5' ) );
+         System.out.printf( "%c \t \t %d\n", '6', ( (int) '6' ) );
+         System.out.printf( "%c \t \t %d\n", '7', ( (int) '7' ) );
+         System.out.printf( "%c \t \t %d\n", '8', ( (int) '8' ) );
+         System.out.printf( "%c \t \t %d\n", '9', ( (int) '9' ) );
+         
+         System.out.println("\n");
+         System.out.println("Special symbols");
+         System.out.println("Character \t Value");
+         System.out.printf( "%c \t \t %d\n", '+', ( (int) '+' ) );
+         System.out.printf( "%c \t \t %d\n", '-', ( (int) '-' ) );
+         System.out.printf( "%c \t \t %d\n", '_', ( (int) '_' ) );
+         System.out.printf( "%c \t \t %d\n", '=', ( (int) '=' ) );
+         System.out.printf( "%c \t \t %d\n", '&', ( (int) '&' ) );
+         System.out.printf( "%c \t \t %d\n", '@', ( (int) '@' ) );
+         System.out.printf( "%c \t \t %d\n", '#', ( (int) '#' ) );
+         System.out.printf( "%c \t \t %d\n", '$', ( (int) '$' ) );
+         System.out.printf( "%c \t \t %d\n", '?', ( (int) '?' ) );
+         System.out.printf( "%c \t \t %d\n", '<', ( (int) '<' ) );
+         System.out.printf( "%c \t \t %d\n", '>', ( (int) '>' ) );
+         System.out.printf( "%c \t \t %d\n", '|', ( (int) '|' ) );
+         System.out.printf( "%c \t \t %d\n", '!', ( (int) '!' ) );
+         System.out.printf( "%c \t \t %d\n", '~', ( (int) '~' ) );
+         System.out.printf( "%c \t \t %d\n", ' ', ( (int) ' ' ) );
+     }
+}
