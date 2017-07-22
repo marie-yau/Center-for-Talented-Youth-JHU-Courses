c8f543b7cec42e0ff6addfb2345860cd77ba86cd
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-07-22 08:26:04 +0200

    Add files via upload

---
+++ Unit1Lab1.java
@@ -0,0 +1,21 @@
+/**
+* This program displays four special characters.
+* @author Marie Dohnalová
+*
+*/
+public class Unit1Lab1 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("*********     ***       *        *");
+        System.out.println("*       *   *     *    ***      * *");
+        System.out.println("*       *  *       *  *****    *   *");
+        System.out.println("*       *  *       *    *     *     *");
+        System.out.println("*       *  *       *    *    *       *");
+        System.out.println("*       *  *       *    *     *     *");
+        System.out.println("*       *  *       *    *      *   *");
+        System.out.println("*       *   *     *     *       * *");
+        System.out.println("*       *     ***       *        *");
+    }
+    
+}
