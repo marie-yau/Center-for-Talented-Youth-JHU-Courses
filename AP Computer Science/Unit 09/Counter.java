77e7a137c27279d8bf05c1c9fa87bcbe58e6cead
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-10-09 02:33:43 -0700

    Add files via upload

---
+++ AP Computer Science/Unit 09/Counter.java
@@ -0,0 +1,42 @@
+
+public class Counter {
+	private int count;
+        
+	
+        /**
+        * Precondition: none (class has to have the same name as constructor)
+        * Postcondition: variable count is initialized to 0
+        */	
+	public Counter(){
+		count=0;
+	}
+        
+	// Getter method
+	public int getCount(){
+		return count;
+	}
+        
+        /**
+         * Precondition: none
+         * Postcondition: value of variable count is increased by 1
+         */	
+	public void increment(){
+		count++;
+	}
+
+        /**
+         * Precondition: none
+         * Postcondition: value of variable count is decreased by 1
+         */	        
+	public void decrement(){
+		count--;
+	}
+
+        /**
+         * Precondition: none
+         * Postcondition: value of variable count is set to 0
+         */	        
+	public void reset(){
+		count=0;
+	}
+}
