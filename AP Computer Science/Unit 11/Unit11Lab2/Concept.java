2946f00bd117038fffc05a565eed85d7b7f94620
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-11-14 02:42:09 -0800

    Inheritance - Part 2

---
+++ AP Computer Science/Unit 11/Unit11Lab2/Concept.java
@@ -0,0 +1,15 @@
+public abstract class Concept implements Drawable, Rotatable, Resizable, Sounds {
+
+        
+	@Override
+	public void playSound() {}
+
+	@Override
+	public void resizeObject() {}
+
+	@Override
+	public void rotateObject() {}
+
+	@Override
+	public void drawObject() {}
+}
