417c8fbe0c2a07c1d2d402140e69439b978a2e08
GeekTheGreat <marie.dohnalova21@gmail.com>
2017-11-26 01:19:44 -0800

    Exception Handling

---
+++ AP Computer Science/Unit 13/Unit13Lab2/NullPointerExceptionThrown.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Marie Dohnalová
+ * This program throws NullPointerException.
+ */
+public class NullPointerExceptionThrown {
+
+	public static void main(String[] args) {
+            
+		String str = null;
+		
+		System.out.println(str.length());
+	}
+
+}
